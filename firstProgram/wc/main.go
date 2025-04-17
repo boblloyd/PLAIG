bcab65478d48729e7419740895d3e8a453a2a9d6
Bob Lloyd <bobthecodebuilder81+github@gmail.com>
2025-04-17 10:51:07 -0400

    Add tests for mdp with a template file.  Update the wc program to handle counting words in a single file.

--- firstProgram/wc/main.go
+++ firstProgram/wc/main.go
@@ -11,13 +11,33 @@ import (
 func main() {
 	lines := flag.Bool("l", false, "Count lines")
 	bytes := flag.Bool("b", false, "Count bytes")
+	file := flag.String("f", "", "File to read as input")
 	flag.Parse()
-	fmt.Println(count(os.Stdin, *lines, *bytes))
+
+	i, err := run(*lines, *bytes, *file)
+	if err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(1)
+	}
+	fmt.Println(i)
+}
+
+func run(lines bool, bytes bool, file string) (int, error) {
+	input := os.Stdin
+	if file != "" {
+		fileInput, err := os.Open(file)
+		if err != nil {
+			return 0, err
+		}
+		input = fileInput
+		defer fileInput.Close()
+	}
+	return count(input, lines, bytes), nil
 }
 
 func count(r io.Reader, countLines bool, countBytes bool) int {
 	scanner := bufio.NewScanner(r)
-	if !countLines && !countBytes{
+	if !countLines && !countBytes {
 		scanner.Split(bufio.ScanWords)
 	} else if !countLines {
 		scanner.Split(bufio.ScanBytes)
